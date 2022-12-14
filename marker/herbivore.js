59bd2043459487926d2ce7d88031ee4a38f3c5c1
interactive-game-maps <>
2022-12-14 23:05:23 +0100

    Herbivore chapter III

--- marker/herbivore.js
+++ marker/herbivore.js
@@ -169,6 +169,485 @@ var herbivore = {
                     -56.0625
                 ]
             }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_01"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    107.1875,
+                    -89.0625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "image_id": "III_01",
+                "id": "III_02"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    106.75,
+                    -88.609375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_03"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    109.578125,
+                    -92.359375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_04"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    116.03125,
+                    -95.46875
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_05"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    111.703125,
+                    -90.21875
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_06"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    117.78125,
+                    -93.125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_07"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    134.484375,
+                    -90.734375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_08"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    134.46875,
+                    -96.3125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_09"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    131.84375,
+                    -101.40625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_10"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    133.8125,
+                    -103.78125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "description": "3",
+                "id": "III_11"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    131.828125,
+                    -103.984375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_12"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    114.34375,
+                    -119.484375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_13"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    117.78125,
+                    -120.84375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_14"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    121.65625,
+                    -122.515625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_15"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    122.75,
+                    -127.609375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_16"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    123.140625,
+                    -140.0625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_17"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    112.1875,
+                    -134.125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_18",
+                "description": "Behind the building"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    106.734375,
+                    -146.453125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_19",
+                "description": "3"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    100.171875,
+                    -129.34375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_20"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    102.796875,
+                    -129.21875
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_21"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    87.703125,
+                    -125.78125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_22",
+                "description": "2"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    81.203125,
+                    -125.890625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_23"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    62.28125,
+                    -120.796875
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_24",
+                "description": "3"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    62.90625,
+                    -120.78125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_25"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    63.484375,
+                    -120.796875
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_26"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    97.59375,
+                    -123.578125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_27"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    107.234375,
+                    -120.75
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_28"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    92.453125,
+                    -118.234375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_29",
+                "description": "3"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    95.859375,
+                    -112.546875
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_30",
+                "description": "On top of the building, requires jumping"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    143.015625,
+                    -121.015625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_31",
+                "description": "3"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    147.109375,
+                    -113.9375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_32"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    146.609375,
+                    -115.34375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_33"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    158.140625,
+                    -109.765625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_34"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    158.90625,
+                    -90.375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_35",
+                "description": "3"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    149.125,
+                    -85.84375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "III_36",
+                "description": "Requires finding all glass shards"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    141.1875,
+                    -102.125
+                ]
+            }
         }
     ]
 };
