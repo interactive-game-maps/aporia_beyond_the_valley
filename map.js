53e083c83ecfadf533e74e2bc55dc2a18441dcb9
interactive-game-maps <>
2022-12-17 23:00:10 +0100

    Clean up template remainders

--- map.js
+++ map.js
@@ -1,10 +1,5 @@
 // Step 0:
 // Add all feature geoJSON and layer logic to the `index.html`
-// In this example this is:
-// * `marker/collectibles.js`
-// * `marker/information.js`
-// * `marker_logic/collectibles.js`
-// * `marker_logic/information.js`
 
 // Step 1:
 // Initialize the map with basic information
@@ -22,48 +17,26 @@ var interactive_map = new InteractiveMap('map', {
 
 // Step 2:
 // Add at least one tile layer
-//
-// generate them from an image with (don't forget do adjust the zoom levels `-z`):
-// https://github.com/commenthol/gdal2tiles-leaflet
-// `./gdal2tiles.py -l -p raster -w none -z 3-5 full_map.jpg map_tiles`
 interactive_map.addTileLayer('Ingame map', {
     minNativeZoom: 2,
     maxNativeZoom: 5,
     attribution: 'Map from <a href="https://aporiathegame.com/">Invisible Walls</a> (game data files)'
 });
 
-// Step 2.5 (optional):
-// Add more tile layer
-// interactive_map.addTileLayer('Overview', {
-//     minNativeZoom: 2,
-//     maxNativeZoom: 4,
-//     attribution: 'Map from <a href="https://www.example.com/index.html">$source</a>'
-// }, 'overview_tiles/{z}/{x}/{y}.png');
-
 // Step 3:
 // Add at least one marker layer
-// The order matters - they will appear in this order in the sidebar and layer control
-// See `marker_logic/collectibles.js` for a really basic layer
 addAlcoholic(interactive_map);
 
 // Step 3.5 (optional):
-// Add more marker layer
-// See `marker_logic/information.js` for more advanced technics
 addChapter(interactive_map);
 
 // Step 3.5 (optional):
-// Add more marker layer
-// See `marker_logic/information.js` for more advanced technics
 addGlassShards(interactive_map);
 
 // Step 3.5 (optional):
-// Add more marker layer
-// See `marker_logic/information.js` for more advanced technics
 addHerbivore(interactive_map);
 
 // Step 3.5 (optional):
-// Add more marker layer
-// See `marker_logic/information.js` for more advanced technics
 addLightMyFire(interactive_map);
 
 // Step 4:
